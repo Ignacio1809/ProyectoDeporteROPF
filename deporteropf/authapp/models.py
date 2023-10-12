e5088f21b3a42683d85e2571c5e7d88e6c694b23
Esteban-Riquelme <estebanriquelme605@gmail.com>
2023-10-12 01:26:08 -0300

    cambios en base de datos

--- deporteropf/authapp/models.py
+++ deporteropf/authapp/models.py
@@ -4,3 +4,11 @@ from django.db import models
 #en este archivo crearemos las tablas de nuestra base de datos 
 
 
+class Plan(models.Model):
+    nombre_plan = models.CharField(max_length=20)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    duracion =  models.CharField(max_length=20)
+    beneficio= models.TextField()
+    def __str__(self):
+        return self.nombre_plan
