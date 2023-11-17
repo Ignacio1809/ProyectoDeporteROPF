792c3f950769d38b6b82575b19750f67012b4ec9
Ignacio1809 <ig.orellanah@duocuc.cl>
2023-11-17 13:33:25 -0300

    se sube actualizado el pago

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.models import User
 import re
 from .models import *
@@ -76,6 +76,8 @@ def signup(request):
         user = User.objects.create_user(username=name, email=email, password=pass1)
         usuario = cliente.objects.create(
             telefono=number, nombre=name, apellido=lname, email=email, estatura=height, peso=weight, imc=imc, fecha_nac=birth, passwrd=pass1)
+        
+        request.session['user_email'] = email
 
         messages.info(request, "Usuario creado con éxito.")
         return redirect('/login')
@@ -113,14 +115,22 @@ def validate_password_strength(value):
         raise ValidationError('La contraseña debe contener al menos un símbolo.')
 
 def create_payment(request, plan_id):
+    if not request.user.is_authenticated:
+        # Si el usuario no está autenticado, redirígelo a la página de inicio de sesión
+        messages.error(request, "Por favor, inicia sesión para continuar.")
+        return redirect('login')
+    
     if request.method == "POST":
-        #Vamos a traer el plan seleccionado
+        # Traer el plan seleccionado basado en plan_id
         plan = get_object_or_404(Plan, pk=plan_id)
-
-        #Instanciar el SDK de Mercado Pago con el access token 
+        
+        # Almacenar el plan_id en la sesión para usar después del pago
+        request.session['plan_id'] = plan_id
+        
+        # Instanciar el SDK de Mercado Pago con el access token
         sdk = SDK("TEST-5942360496032135-110712-dc70db76f8f00eab7b6f1684b9415b8d-1536335135")
-
-        #Crear datos de la preferencia de pago
+        
+        # Crear datos de la preferencia de pago
         preference_data = {
             "items": [
                 {
@@ -132,69 +142,82 @@ def create_payment(request, plan_id):
                 }
             ],
             "back_urls": {
-                "success":"http://127.0.0.1:8000/profile/",
-                "failure":"http://127.0.0.1:8000/pago_fallido/",
-                "pending":"http://127.0.0.1:8000/pago_pendiente/",
-            }
+                "success": "http://127.0.0.1:8000/payment_success/",
+                "failure": "http://127.0.0.1:8000/pago_fallido/",
+            },
+            # Asumiendo que quieres pasar el email del usuario como referencia externa
+            "external_reference": request.user.email,
         }
-
+        
+        # Crear la preferencia de pago en Mercado Pago
         preference_response = sdk.preference().create(preference_data)
-
+        
         # Obtener el enlace de pago desde la respuesta
         payment_link = preference_response["response"]["init_point"]
-
-        # Redirigir al usuario al enlace de pago
         
+        # Redirigir al usuario al enlace de pago
         return redirect(payment_link)
     
-
     else:
         # Si no es POST, simplemente mostramos la página con el formulario de pago
+        plan = get_object_or_404(Plan, pk=plan_id)
         return render(request, "plan_detail.html", {'p': plan})
     
 
 def payment_success(request):
-    email_cliente = request.GET.get('email_cliente')
-    plan_id = request.GET.get('plan_id')
+    # Asumiendo que usas la autenticación de Django y que el email del usuario está en la sesión.
+    if request.user.is_authenticated:
+        # Obteniendo email y plan_id de la sesión en lugar de la URL para mayor seguridad.
+        email_cliente = request.session.get('user_email', request.user.email)
+        plan_id = request.session.get('plan_id')
+        
+        if not plan_id:
+            messages.error(request, "No se pudo completar la compra. Falta información del plan.")
+            return redirect('pago_fallido')
 
-    if not email_cliente or not plan_id:
-        # Agregar manejo de error si falta algún parámetro
-        messages.error(request, "Información de pago incompleta.")
-        return redirect('ruta_error')  # Asegúrate de definir esta ruta
+        try:
+            cliente_obj = get_object_or_404(cliente, email=email_cliente)
+            plan_obj = get_object_or_404(Plan, pk=plan_id)
 
-    try:
-        cliente_obj = get_object_or_404(cliente, email=email_cliente)
-        plan_obj = get_object_or_404(Plan, pk=plan_id)
+            cliente_obj.plan_actual = plan_obj
+            cliente_obj.save()
 
-        cliente_obj.plan_actual = plan_obj
-        cliente_obj.save()
+            # Limpiando la información de la sesión después de usarla.
+            del request.session['plan_id']
+            del request.session['user_email']
 
-        messages.success(request, "Compra realizada con éxito.")
-        return redirect("profile.html")  
-    except Exception as e:
-        # Agregar manejo de excepciones más detallado
-        messages.error(request, str(e))
-        return redirect('ruta_error')
+            messages.success(request, "Compra realizada con éxito.")
+            return redirect('profile')  # Redirige a la ruta de nombre 'profile'
+        except cliente.DoesNotExist:
+            messages.error(request, "No se encontró el cliente.")
+            return redirect('pago_fallido')
+        except Plan.DoesNotExist:
+            messages.error(request, "No se encontró el plan.")
+            return redirect('pago_fallido')
+        except Exception as e:
+            # Agregar manejo de excepciones más detallado
+            messages.error(request, str(e))
+            return redirect('pago_fallido')
+    else:
+        messages.error(request, "Usuario no autenticado.")
+        return redirect('login')
+
+User = get_user_model()
 
 def profile(request):
-    user = request.user
-    if not user.is_authenticated:
-        # Si el usuario no está autenticado, redirígelo al inicio de sesión o registro
-        return redirect('handlelogin.html')
+    if not request.user.is_authenticated:
+        # Redirige al nombre de la ruta de inicio de sesión
+        return redirect('nombre_de_la_ruta_de_login')  # Asegúrate de que esto corresponda con tus urls.py
 
     try:
-        cliente_obj = cliente.objects.get(email=user.email)
+        # Aquí usas User para realizar operaciones relacionadas con el modelo de usuario si es necesario.
+        cliente_obj = get_object_or_404(cliente, email=request.user.email)
         plan_actual = cliente_obj.plan_actual
         return render(request, "profile.html", {'cliente': cliente_obj, 'plan': plan_actual})
     except cliente.DoesNotExist:
-        # Manejar el caso de que el cliente no exista
-        return render(request, "error.html", {'mensaje': "Perfil no encontrado."})
-
-
+        messages.error(request, "Perfil no encontrado.")
+        return redirect('login')  # Puede ser la página de inicio o cualquier otra
 # Redirigir a las páginas de éxito y fracaso del pago
 
 def pago_fallido(request):
     return render(request, "pago_fallido.html")
-
-def pago_pendiente(request):
-    return render (request, "pago_pendiente.html")
